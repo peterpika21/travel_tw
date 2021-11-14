2643c9d5b87eb68bddf83aaed345e605714345b5
I-CHIEN <peterpika21@gmail.com>
2021-11-14 16:52:54 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import viteSvgIcons from 'vite-plugin-svg-icons';
 import path from 'path'; // ts如果报错 npm i @types/node -D
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteSvgIcons({
-      // 配置路劲在你的src里的svg存放文件
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ],
-  base: process.env.BASE, // 對應github倉庫名稱
-  resolve: {
-    alias: {
-      '@': `${path.resolve(__dirname, 'src')}/`
+export default (ctx: any) => {
+  process.env = { ...process.env, ...loadEnv(ctx.mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      viteSvgIcons({
+        // 配置路劲在你的src里的svg存放文件
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]'
+      })
+    ],
+    base: process.env.VITE_BASE, // 對應github倉庫名稱
+    resolve: {
+      alias: {
+        '@': `${path.resolve(__dirname, 'src')}/`
+      }
     }
-  }
-});
+  });
+};
